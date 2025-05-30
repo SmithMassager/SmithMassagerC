fb4c06be0ef95043436cd4906826c39f04dff428
ziwen <zw6wang@uwaterloo.ca>
2025-05-30 19:07:20 -0400

    Add include

---
+++ include/maple_call.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "gmp.h"
+// If you get compilation error try the following name. Depending on the version of maple the header file name might be changed.
+//#include "maple_c.h"
+#include "maplec.h"
+
+/*
+ * Args:  1
+ *        A - nxn RTable
+ */
+ALGEB M_DECL highOrderResidue_maple(MKernelVector kv, ALGEB args);
+/*
+ * Args:  1
+ *        A - nxn RTable
+ */
+ALGEB M_DECL unicert_maple(MKernelVector kv, ALGEB args);
+
+/*
+ * Args:  2
+ *        A - nxn RTable
+ *        b - 1xn RTable
+ */
+ALGEB M_DECL horSolveIML_maple(MKernelVector kv, ALGEB args);
+ALGEB M_DECL horSolveSpinv_maple(MKernelVector kv, ALGEB args);
+ALGEB M_DECL imlSolve_maple(MKernelVector kv, ALGEB args);
+
+ALGEB M_DECL maple_passing(MKernelVector kv, ALGEB args);
+
+ALGEB M_DECL iherm_maple(MKernelVector kv, ALGEB args);
